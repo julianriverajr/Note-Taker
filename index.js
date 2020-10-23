c737ff9cfd4eb176d1a0eae6f8367846d2bea2f2
Julian Rivera Jr <julianriverajr@gmail.com>
2020-10-22 23:53:47 -0500

    restructured code

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ const PORT = process.env.PORT || 3036;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// yo
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
+// yo
+
+
 
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};

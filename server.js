eb50a1cc37cbbfe90d27c0bf4228091f1639ade6
Julian Rivera Jr <julianriverajr@gmail.com>
2020-12-01 22:46:13 -0600

    added server and routes

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("*", function(req,res){
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+app.get("/notes", function(req,res){
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", function(req,res){
+    res.json(JSON.parse(fs.readFileSync("./db/db.json", "utf8")));
+});
+app.post("/api/notes", function(req,res){
+    var note = req.body;
+    note.id = (Math.floor(Math.random()*100) + (Math.floor(Math.random()*100)))
+    let notesArray = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    notesArray.push(note);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notesArray), "utf8");
+    res.json(notesArray);;
+})
+app.delete("/api/notes/:id", function(req,res){
+    const {id} = req.params;
+    let notesArray = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    for (let i = 0; i<notesArray.length; i++){
+        if (notesArray[i].id === id){
+            notesArray.splice(i,1);
+            break;
+        }
+    }
+    fs.writeFileSync("./db/db.json", JSON.stringify(notesArray), "utf8");
+    res.json(notesArray);
+});
+app.listen(PORT, () => console.log("App listening on PORT: " + PORT));
